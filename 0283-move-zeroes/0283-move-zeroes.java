90944baaa202bcbcc1ed821c68d9d4f189a38148
Navinandyal <89778837+Navinandyal@users.noreply.github.com>
2026-08-24 07:42:41 +0530

    Time: 2 ms (91.96%), Space: 46.2 MB (99.94%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0,j=0;
+        for(;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j++]=temp;
+            }
+        }
+    }
+}
